1acb2e24061780c1f3f0455d997c5b6baa238b5d
Lukas Stucki <lukas.stucki@logobject.ch>
2025-03-11 17:16:01 +0100

    moved

--- lib/subtask_list_enhanced_hook.rb
+++ lib/subtask_list_enhanced_hook.rb
@@ -1,3 +1,3 @@
-class RedmineSubtaskListEnhancedHook < Redmine::Hook::ViewListener
+class SubtaskListEnhancedHook < Redmine::Hook::ViewListener
   render_on :view_issues_show_description_bottom, :partial => 'issues/subtask_list_enhanced_partial'
 end
