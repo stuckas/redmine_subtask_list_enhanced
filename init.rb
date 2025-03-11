d40a4b3d62eee2035fed6f7b070e8beaa077bf22
Lukas Stucki <lukas.stucki@logobject.ch>
2025-03-11 17:19:32 +0100

    moved

--- init.rb
+++ init.rb
@@ -7,11 +7,4 @@ Redmine::Plugin.register :redmine_subtask_list_enhanced do
   author_url 'https://github.com/ekho'
 end
 
-if Rails.version > '6.0'
-  if Rails.configuration.respond_to?(:autoloader) && Rails.configuration.autoloader == :zeitwerk
-    Rails.autoloaders.each { |loader| loader.ignore(File.dirname(__FILE__) + '/lib') }
-  end
-  require_dependency File.dirname(__FILE__) + '/lib/redmine_subtask_list_enhanced/hooks/subtask_list_enhanced_hook'
-else
-  require_dependency 'redmine_subtask_list_enhanced/hooks/subtask_list_enhanced_hook'
-end
+require_relative 'lib/subtask_list_enhanced_hook'
